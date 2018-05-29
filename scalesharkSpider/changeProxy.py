bba78962b2df12306b531c10b21572e616586f3e
yy189 <yyuan@cs.duke.edu>
2018-05-29 17:06:48 -0400

    今天大概写完了多进程验证代理。。。

--- scalesharkSpider/changeProxy.py
+++ scalesharkSpider/changeProxy.py
@@ -1,6 +1,19 @@
+# -*- coding:utf-8 -*-
+
 import requests
 import urllib2
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+import copy_reg
+import types
+
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+copy_reg.pickle(types.MethodType, _pickle_method)
 
 class ChangeProxy(object):
     
@@ -8,55 +21,67 @@ class ChangeProxy(object):
         self.num = 300
         self.url = "https://free-proxy-list.net/"
         self.header = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'}
-        self.file = open("proxy.py", "wb")
-        # self.Lock = threading.Lock()
-        # self.ip_list = []
-        
-        self.count = 0
-        self.evecount = 0
-        
-    def getIPData(self):
-        # del self.ip_list [:]
+        self.file = open("proxy.py", "w")
+        self.file.write("PROXIES = [\n]")
+        self.tmp_proxies = []
+
+    def get_ip(self):
+        del self.tmp_proxies [:]
         res = requests.get(self.url, headers=self.header)
         soup = BeautifulSoup(res.text, 'lxml')
         tr_list = soup.find_all('tbody')[0].find_all('tr')
         count = 0
-        self.file.write("PROXIES = [\n") #这里要改吧。。。想实现多线程
         for each in tr_list:
-            if count < self.num:
+            if count < self.num: # 一共300个ip
                 ips = each.find_all('td')
                 if ips[6].getText() == 'yes':
                     prtcl = "https"
                 else:
                     prtcl = "http"
                 ip_port = str(ips[0].contents[0]) + ":" + str(ips[1].contents[0])
-                self.f.write("\t\t\t{" + "'prtcl':'" + prtcl + "', 'ip_port':'" + ip_port + "'},\n")
+                # self.f.write("\t\t\t{" + "'prtcl':'" + prtcl + "', 'ip_port':'" + ip_port + "'},\n")
+                self.tmp_proxies.append({'prtcl': prtcl, 'ip_port': ip_port})
                 count += 1
-        self.file.write("]\n")
+        print("ip got!")
+        # self.file.write("]\n")
         # f.close()
 
-    def _is_alive(self, prtcl, ip_port):
-        proxy = {prtcl:ip_port}
-        print(proxy)
+    # 失败了在程序中删吧...
+    def verify_ip(self, prtcl_ip_port):
+        proxy = {prtcl_ip_port['prtcl']:prtcl_ip_port['ip_port']}
 
         proxy_support = urllib2.ProxyHandler(proxy)
         opener = urllib2.build_opener(proxy_support)
         urllib2.install_opener(opener)
 
-        test_url = "https://www.crunchbase.com/"
+        test_url = "https://www.google.com/"
         req = urllib2.Request(test_url, headers = self.header)
         try:
-            resp = urllib2.urlopen(req, timeout = 10) #proxy delay 10s
+            resp = urllib2.urlopen(req, timeout = 1) #proxy delay 1s
 
             if resp.code == 200:
-                print("work")
-                return True
-            else:
-                print("not work")
-                return False
+                return "\t\t\t{" + "'prtcl':'" + prtcl_ip_port['prtcl'] + "', 'ip_port':'" + prtcl_ip_port['ip_port'] + "'},\n]"
+
         except:
-            print("not work")
-            return False
+            pass
+
+    def mycallback(self, x):
+        if x != None:
+            self.file.seek(-1,2)
+            self.file.write(x)
+            self.file.flush()
+
+    def main(self):
+        self.get_ip()
+        p = Pool(4)
+
+        for i in range(len(self.tmp_proxies)):
+            handler = p.apply_async(self.verify_ip, args=(self.tmp_proxies[i],), callback=self.mycallback)
+            print(handler.get())
+
+        p.close()
+        p.join()
+        self.file.close()
 
     # def changeProxy(self, request):
     #     request.meta["proxy"] = self.ip_list[self.count-1]["prtcl"] + "://" + self.ip_list[self.count-1]["ip_port"]
@@ -88,9 +113,7 @@ class ChangeProxy(object):
     #         self.evecount = self.evecount + 1
     #
     #     self.ifUsed(request)
-    def close_file(self):
-        self.file.close()
 
 if __name__ == "__main__":
     changeProxy = ChangeProxy()
-    changeProxy.getIPData()
+    changeProxy.main()
